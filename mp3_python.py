16ee3d96fac75a8ae78a385229a7801cf2f1f725
shendriksza <shendriks.za@gmail.com>
2017-11-15 18:11:37 +0000

    Add basic state machine

--- mp3_python.py
+++ mp3_python.py
@@ -1,12 +1,36 @@
 #! /usr/bin/python
 import vlc
 import time
+import random
+import os
+
+PLAY  = 0
+PAUSE = 1
+NEXT  = 2
+PREV  = 3
+QUIT  = 4
 
 if __name__ == '__main__':
+
     instance = vlc.Instance()
     player = instance.media_player_new()
-    media = instance.media_new("Kalimba.mp3")
-    player.set_media(media)
-    player.play()
-    time.sleep(10)
+
+    state = PLAY
+    mnt_root = '/media/usb'
+    mnt_files = [instance.media_new(os.path.join(mnt_root,x)) for x in os.listdir(mnt_root) if x.endswith('.mp3')]
+    random.shuffle(mnt_files)
+    player.set_media(mnt_files[0])
+
+    while(state!=QUIT):
+        if(state==PLAY):
+            player.play()
+        if(state==NEXT):
+            player.set_media(mnt_files[1])
+            state=PLAY
+        if(state==PAUSE):
+            player.pause()
+        try:
+            state = int(raw_input())
+        except:
+            pass
     player.stop()
